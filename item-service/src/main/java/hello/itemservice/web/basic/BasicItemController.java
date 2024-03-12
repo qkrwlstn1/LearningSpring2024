d19ac13362d4237a48dd57c83b8e6ec5590698d9
qkrwlstn1 <aldhfldns@naver.com>
2024-03-12 17:56:20 +0900

    create web

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -39,6 +40,41 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm (){
+        return "basic/addForm";
+    }
+    @PostMapping("/add")
+    //public String save (@RequestParam String itemName,
+    //                      @RequestParam int price,
+    //                      @RequestParam Integer quantity,
+    //                      Model model){
+    // 직접 Item에 직접 값을 세팅해주고 redirect 없이 /basic/items로 가는 코드
+    //public String save (@ModelAttribute("item") Item item
+    // model.addAttribute("item", item) 의 기능을 해줌
+    public String save (Item item, Model model, RedirectAttributes ra){
+
+        itemRepository.save(item);
+        ra.addAttribute("itemId",item.getId());
+        ra.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable long itemId, Model model, Item item){
+
+        itemRepository.update(itemId, item);
+        model.addAttribute("item", item);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
